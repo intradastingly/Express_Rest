5a786273245dc18738c3d37b4c6f09810b676072
Intradastingly <asaf_lackgren@hotmail.com>
2021-04-08 15:46:08 +0200

    now it work

--- public/script.js
+++ public/script.js
@@ -5,13 +5,15 @@ window.addEventListener('load', initSite);
 async function initSite() {
     console.log('init')
     /* getSpecificUserData('5682c6dc-779d-4752-a4cd-a94110afce2a') */
+   /*  
+    const UserData = await getSpecificUserData('b59a594e-a244-4819-8d8b-a7fc74261014') */
+    const deleted = await deleteUser('f58bd98d-8856-467f-b619-fe1588bdc174')
     const allData = await getAlluserData()
-    const UserData = await getSpecificUserData('b59a594e-a244-4819-8d8b-a7fc74261014')
-    const deleted = await getSpecificUserData('5682c6dc-779d-4752-a4cd-a94110afce2a')
-    const stuff = await saveNewUser("asaf", 'haha', 'w')
-    console.log(allData)
+    /* const stuff = await saveNewUser("asaf", 'haha', 'w') */
+    /*
     console.log(UserData)
-    console.log(stuff)
+    console.log(stuff) */
+    console.log(allData)
     console.log(deleted)
 }
 
