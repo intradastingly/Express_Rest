833506ddf7223f6bdf16f2581f739ed411bb5b77
Intradastingly <asaf_lackgren@hotmail.com>
2021-04-13 10:49:10 +0200

    some html and css and scripts

--- public/script.js
+++ public/script.js
@@ -3,23 +3,52 @@
 window.addEventListener('load', initSite);
 
 async function initSite() {
-    console.log('init')
+    showData()
+    eventListeners()
     /* getSpecificUserData('5682c6dc-779d-4752-a4cd-a94110afce2a') */
    /*  
     const UserData = await getSpecificUserData('b59a594e-a244-4819-8d8b-a7fc74261014') */
-    const deleted = await deleteUser('f58bd98d-8856-467f-b619-fe1588bdc174')
-    const allData = await getAlluserData()
+    /* const deleted = await deleteUser('f58bd98d-8856-467f-b619-fe1588bdc174')
+    const allData = await getAlluserData() */
     /* const stuff = await saveNewUser("asaf", 'haha', 'w') */
     /*
     console.log(UserData)
     console.log(stuff) */
-    console.log(allData)
-    console.log(deleted)
+    /* console.log(allData)
+    console.log(deleted) */
 }
 
-async function getAlluserData() {
-    const userData = await makeRequest("/api/users", "GET")
-    return userData
+let selected = false;
+
+function eventListeners(){    
+    const card = document.getElementsByClassName('container')
+    card.onclick = selectCard;
+}
+
+
+async function showData() {
+    const allData = await makeRequest("/api/users", "GET")
+    for(const user of allData){
+        const userDiv = document.getElementById('allData')
+        const containerDiv = document.createElement('div');
+        const name = document.createElement('p');
+        const email = document.createElement('p');
+        const status = document.createElement('p');
+        containerDiv.className = 'container'
+        name.innerHTML = user.name;
+        email.innerHTML = user.email;
+        status.innerHTML = user.status;
+        containerDiv.appendChild(name)
+        containerDiv.appendChild(email)
+        containerDiv.appendChild(status)
+        userDiv.appendChild(containerDiv)    
+    }
+}
+
+
+
+function selectCard(){
+    console.log('test')
 }
 
 async function getSpecificUserData(id) {
@@ -38,6 +67,7 @@ async function saveNewUser(name, email, dead) {
 }
 
 async function deleteUser(id) {
+    const deleteButton = document.getElementById('')
     const deleted = await makeRequest("/api/users/" + id, "DELETE")
     return deleted
 }
