d72062306251467bd8431da005a4cd818ddb5a04
Intradastingly <asaf_lackgren@hotmail.com>
2021-04-13 13:42:55 +0200

    delete works

--- public/script.js
+++ public/script.js
@@ -1,54 +1,57 @@
 
-
 window.addEventListener('load', initSite);
 
 async function initSite() {
-    showData()
+    populate()
     eventListeners()
-    /* getSpecificUserData('5682c6dc-779d-4752-a4cd-a94110afce2a') */
-   /*  
-    const UserData = await getSpecificUserData('b59a594e-a244-4819-8d8b-a7fc74261014') */
-    /* const deleted = await deleteUser('f58bd98d-8856-467f-b619-fe1588bdc174')
-    const allData = await getAlluserData() */
-    /* const stuff = await saveNewUser("asaf", 'haha', 'w') */
-    /*
-    console.log(UserData)
-    console.log(stuff) */
-    /* console.log(allData)
-    console.log(deleted) */
+    deleteSelected()
 }
 
-let selected = false;
-
-function eventListeners(){    
+function eventListeners(){ 
     const card = document.getElementsByClassName('container')
-    card.onclick = selectCard;
+    setTimeout(()=> {
+        for(const cards of card){cards.addEventListener('click', selectCard)}
+    }, 50) 
 }
 
-
-async function showData() {
+async function populate() {
     const allData = await makeRequest("/api/users", "GET")
-    for(const user of allData){
+    allData.map(i => {
         const userDiv = document.getElementById('allData')
         const containerDiv = document.createElement('div');
-        const name = document.createElement('p');
-        const email = document.createElement('p');
-        const status = document.createElement('p');
-        containerDiv.className = 'container'
-        name.innerHTML = user.name;
-        email.innerHTML = user.email;
-        status.innerHTML = user.status;
+        const name = document.createElement('li');
+        const email = document.createElement('li');
+        const status = document.createElement('li');
+        containerDiv.className = 'container';
+        containerDiv.id = i.id;
+        name.innerHTML = "name: " + i.name;
+        email.innerHTML = "email: " + i.email;
+        status.innerHTML = "status: "+ i.status;
         containerDiv.appendChild(name)
         containerDiv.appendChild(email)
         containerDiv.appendChild(status)
         userDiv.appendChild(containerDiv)    
+    })
+}
+
+async function selectCard(event){
+    const cards = document.getElementsByClassName('container')
+    for(const card of cards){
+        if(event.target.id === card.id){
+            card.classList.add('highlighted')
+            deleteUser(card.id)
+        } else {
+            card.classList.remove('highlighted')
+        }
     }
 }
 
-
-
-function selectCard(){
-    console.log('test')
+async function deleteUser(id) {
+    const deleteUserr = document.getElementById('deleteButton')
+    deleteUserr.addEventListener('click', async () => {
+        await makeRequest("/api/users/" + id, "DELETE")
+        location.reload();
+    }) 
 }
 
 async function getSpecificUserData(id) {
@@ -66,11 +69,7 @@ async function saveNewUser(name, email, dead) {
     return stuff
 }
 
-async function deleteUser(id) {
-    const deleteButton = document.getElementById('')
-    const deleted = await makeRequest("/api/users/" + id, "DELETE")
-    return deleted
-}
+
 
 async function makeRequest(url,method,body){
    
